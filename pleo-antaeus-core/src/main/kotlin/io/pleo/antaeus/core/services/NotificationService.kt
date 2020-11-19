bc88a1d59f6d1415b2bf88a48c9615dc48f010cb
abdoolly <abdallahgamal@Abdallahs-MacBook-Pro.local>
2020-11-19 15:14:23 +0200

    added some logging instead of real notifications in the notificationService

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/NotificationService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/NotificationService.kt
@@ -5,10 +5,14 @@ class NotificationService {
     /**
      * a function that should notify a customer
      */
-    fun notifyCustomer(customerId: Int) {}
+    fun notifyCustomer(customerId: Int, message: String) {
+        println("err for customer $customerId: $message")
+    }
 
     /**
      * a function that should notify an admin
      */
-    fun notifyAdmin() {}
+    fun notifyAdmin(message: String) {
+        println(message)
+    }
 }
