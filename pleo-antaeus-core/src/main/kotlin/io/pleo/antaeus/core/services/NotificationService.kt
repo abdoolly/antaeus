95624f57f45fbee8ffdac6c27bd9600d6de3d25d
abdoolly <abdallahgamal@Abdallahs-MacBook-Pro.local>
2020-11-19 11:11:26 +0200

    added notification service

---
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/NotificationService.kt
@@ -0,0 +1,14 @@
+package io.pleo.antaeus.core.services
+
+class NotificationService {
+
+    /**
+     * a function that should notify a customer
+     */
+    fun notifyCustomer(customerId: Int) {}
+
+    /**
+     * a function that should notify an admin
+     */
+    fun notifyAdmin() {}
+}
